a826e9453c7d1ffc910c14280b6e54cb0ce0639f
viana-rhs <143923147+viana-rhs@users.noreply.github.com>
2024-11-22 10:27:00 +0330

    Add files via upload

---
+++ sevensegmentnumber1to10/sevsegtm.ino
@@ -0,0 +1,35 @@
+int sequence[][7] = {
+  {1, 1, 1, 1, 1, 1, 0},
+  {0, 1, 1, 0, 0, 0, 0},
+  {1, 1, 0, 1, 1, 0, 1},
+  {1, 1, 1, 1, 0, 0, 1},
+  {0, 1, 1, 0, 0, 1, 1},
+  {1, 0, 1, 1, 0, 1, 1},
+  {1, 0, 1, 1, 1, 1, 1},
+  {1, 1, 1, 0, 0, 0, 0},
+  {1, 1, 1, 1, 1, 1, 1},
+  {1, 1, 1, 1, 0, 1, 1},
+};
+void printNumber(int number) {
+  for (int i = 0; i < 7; i++) {
+    digitalWrite(1 + i, sequence[number][i]);
+
+  }
+
+}
+
+void setup() {
+for(int i=0;i<=7;i++){
+  pinMode(i,OUTPUT);
+  }
+
+}
+
+void loop() {
+  for (int i = 0; i < 10; i++) {
+    printNumber(i);
+    delay(1000);
+
+  }
+
+}
